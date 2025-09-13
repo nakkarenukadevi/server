83fbfab9762967633200dd18198837d674d56241
nakkarenukadevi <renukadevi9724@gmail.com>
2025-09-13 12:39:52 +0530

    installed and used express.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World, I am Balu");
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send([
+    {
+      name: "Yogesh",
+      age: 24,
+    },
+  ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
